78339db1f7abd035111de0076d72800279f2306d
Danya Baron <dbaron@wpi.edu>
2021-02-26 16:56:46 -0500

    added log pics

--- boostrap.R
+++ boostrap.R
@@ -1,17 +1,40 @@
-library(tidyverse)
+# library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(acepack)
+library(Hmisc)
+# install.packages('acepack')
+# install.packages("Hmisc",dependecies=T)
 
-df <- read.csv('errorcalc.csv')
+df <- read.csv('mastererrorcalculations.csv')
 
+# ggplot(mpg, aes(displ, hwy, size = hwy))
 
 # ggplot(aes(vis, log(abs(guess-truepercent), 2) ) ) + geom.point()
 
-d <- ggplot(df(vis, guess)) + geom_point()
+d <- ggplot(df, aes(Vis, ErrorScore)) + 
+    scale_size(0.5) +
+    stat_summary(fun = mean, fun.min = min, fun.max = max,
+                   colour = "red") +
+    coord_flip()
 
-# d + stat_summary(fun.data="mean_cl_boot", colour=red"", size=2)
-
-d + coord_flip()
+print(d)
 
 
+sf <- read.csv('mastererrorcalculationsNONNORM.csv')
+
+# ggplot(mpg, aes(displ, hwy, size = hwy))
+
+# ggplot(aes(vis, log(abs(guess-truepercent), 2) ) ) + geom.point()
+
+s <- ggplot(sf, aes(Vis, ErrorScore)) + 
+  scale_size(0.5) +
+  stat_summary(fun = mean, fun.min = min, fun.max = max,
+               colour = "red") +
+  coord_flip()
+
+print(s)
+
+
+  
